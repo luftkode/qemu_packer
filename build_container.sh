8ec5c63535f21286b7375619a298360e811f47ca
Asger Vibel <avi@skytem.com>
2023-06-20 08:23:53 +0000

    Change build-arg for docker build

--- build_container.sh
+++ build_container.sh
@@ -3,6 +3,4 @@
 # Script for building container with environment for building TEM OS Images
 
 export DOCKER_BUILDKIT=1
-export UID_V=$(id -u)
-export GID_V=$(id -g)
-docker build --build-arg uid=$UID_V --build-arg gid=$GID_V -t airit/qemu_packer .
+docker build --build-arg uid=$(id -u) --build-arg gid=$(id -g)-t airit/qemu_packer .
